63604753d9cbff8f1ed15518ac22d03a8738b406
Kholodaet <kholodaet@icloud.com>
2024-06-08 11:26:39 +0300

    try

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import Profile from './Profile/Profile';
+import userData from '../assets/userData.json';
+import FriendList from './FriendList/FriendList';
+import friends from '../assets/friends.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transaction from '../assets/transaction.json';
+
+export default function App() {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transaction} />
+    </>
+  );
+}
